89a9be4e5d1160b1057235936dc21a3b1ee0ad6b
Diana Arabey <diana.arabey@mail.ru>
2019-10-13 22:02:28 +0300

    add tabs

--- DSP-lab2/src/by/bsuir/poit/dsp/lab2/StartApp.java
+++ DSP-lab2/src/by/bsuir/poit/dsp/lab2/StartApp.java
@@ -1,5 +1,16 @@
 package by.bsuir.poit.dsp.lab2;
 
+import javax.swing.JFrame;
+
 public class StartApp {
 
+	public static void main(String[] args) {
+
+        TabbedPane tp = new TabbedPane();
+        tp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tp.setVisible(true);
+
+        
+
+    }
 }
